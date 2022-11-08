185726dd3c36154b69103fcbc5a9fca902247dc3
rexdev08 <raimundorincong@gmail.com>
2022-11-08 18:31:41 -0300

    quite el logo y centre el contenido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Logo from "./components/Logo.js"
+// import Logo from "./components/Logo.js"
 import ListaDeTareas from "./components/ListaDeTareas.js"
 
 function App() {
   return (
     <div className="App">
-      <Logo />
+      {/* <Logo /> */}
 
       <div className="tareas__lista-principal">
         <h1>Mis Tareas</h1>
